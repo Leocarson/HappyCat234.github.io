6a644c430de362234e53e3a236aeacaf49102bb9
HappyCat234 <carsonKaucher@gmail.com>
2016-09-10 15:45:37 -0400

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   $('body').append('<h1 class = \'text center\'>Welcome to creative central</h1>')
   $('body').append('<h3 class=\'hide text center\'>This is a place to spark creativity</h3>')
-  $('h3').delay('800')
-  $('h3').removeClass('hide')
+  $('h3').toggleClass('hide')
   
 });
