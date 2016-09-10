4b212b0e2e5f482fb2680f520a2fa6bd52527245
HappyCat234 <carsonKaucher@gmail.com>
2016-09-10 13:19:02 -0400

    Create creative.js

---
+++ js/creative.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('body').append("<h1 class='text center'>Welcome to creative central</h1>">)
+  $('h1').append("<h3 class='hide text slide'>This is a place to spark creativity</h3>").delay(400).removeClass('hide').animate({width:'toggle'},350);
+  
+});
